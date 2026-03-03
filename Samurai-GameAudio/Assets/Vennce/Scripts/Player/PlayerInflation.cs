ec5b4f22c08b7444ac807c478f6896427b179e8d
xVennce <145119672+xVennce@users.noreply.github.com>
2026-03-03 15:55:53 +0000

    Added player inflation

--- Samurai-GameAudio/Assets/Vennce/Scripts/Player/PlayerInflation.cs
+++ Samurai-GameAudio/Assets/Vennce/Scripts/Player/PlayerInflation.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+public class PlayerInflation : MonoBehaviour {
+    [Header("Player References")]
+    [SerializeField] private GameObject playerModel;
+    [SerializeField] private Vector3 playerDesiredScale;
+    [SerializeField] private float scaleSpeed = 5f;
 
-public class PlayerInflation : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    private Vector3 playerInitialScale;
+    private Vector3 targetScale;
+
+    [Header("Toggle Check")]
+    [SerializeField] public bool isScaled = false;
+
+    private void Start() {
+        playerInitialScale = playerModel.transform.localScale;
+        targetScale = isScaled ? playerDesiredScale : playerInitialScale;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            isScaled = !isScaled;
+            targetScale = isScaled ? playerDesiredScale : playerInitialScale;
+            Debug.Log("Toggled fat: " + isScaled);
+        }
+
+        playerModel.transform.localScale = Vector3.Lerp(
+            playerModel.transform.localScale,
+            targetScale,
+            Time.deltaTime * scaleSpeed
+        );
     }
 }
