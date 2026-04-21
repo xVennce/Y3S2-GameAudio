1189a2174f361903b01bcd7daa6783dd9e397d2d
SeanAndrews115 <146887805+SeanAndrews115@users.noreply.github.com>
2026-04-21 13:27:56 +0100

    Menu Sounds

---
+++ Samurai-GameAudio/Assets/Scripts/UIHover.cs
@@ -0,0 +1,40 @@
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIHover : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] EventReference uiHoverEvent;
+    [SerializeField] EventReference uiClickEvent;
+
+    public void OnSelect(BaseEventData eventData) //Controller Hover
+    {
+        RuntimeManager.PlayOneShot(uiHoverEvent);
+    }
+    public void OnDeselect(BaseEventData eventData) //Controller Exit
+    {
+    }
+    public void OnPointerEnter(PointerEventData eventData) //Mouse Hover
+    {
+        RuntimeManager.PlayOneShot(uiHoverEvent);
+        print("Mouse Hover Check Working");
+    }
+    public void OnPointerExit(PointerEventData eventData) //Mouse Exit
+    {
+    }
+    public void OnClick()
+    {
+        print("Button Clicked");
+        RuntimeManager.PlayOneShot(uiClickEvent);
+        StartCoroutine("StartGame");
+    }
+
+    IEnumerator StartGame()
+    
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("Demo");
+    }
+}
