67ff04bbd317df1f6344e284addd56f4f32ee29f
JoshElwell <elwell.joshua@yahoo.com>
2026-04-22 19:49:02 +0100

    Added voiceline to man at pond. Imported audio for man at pond
    Added sounds into fmod, made a parameter for man at pond to iterate through. Added a script which still doesnt output the audio

---
+++ Samurai-GameAudio/Assets/Man_at_pond.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+
+public class Man_at_pond_voice : MonoBehaviour
+
+{
+    [SerializeField] private EventReference Man_at_pond;
+    private EventInstance Man_at_pond_Instance;
+    private int Collision_Count;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Man_at_pond_Instance = RuntimeManager.CreateInstance(Man_at_pond);
+        Collision_Count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            
+
+            if (Collision_Count==1)
+            {
+                Collision_Count--;
+                print(Collision_Count);
+
+                Man_at_pond_Instance.setParameterByName("Man at pond", Collision_Count);
+                Man_at_pond_Instance.start();
+            }
+
+            if (Collision_Count ==0)
+            {
+                Collision_Count++;
+                print(Collision_Count);
+
+                Man_at_pond_Instance.setParameterByName("Man at pond", Collision_Count);
+                Man_at_pond_Instance.start();
+            }
+            
+
+
+            print(Collision_Count);
+
+            Man_at_pond_Instance.setParameterByName("Man at pond", Collision_Count);
+            Man_at_pond_Instance.start();
+        }
+    }
+
+    void OnTriggerExit(Collider player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            //Man_at_pond_Instance();
+        }
+    }
+}
